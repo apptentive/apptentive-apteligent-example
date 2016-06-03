b2450893ba778e87b22a26509f68b2275c5778de
Frank Schmitt <frank@apptentive.com>
2016-06-03 10:23:24 -0700

    Add button to crash app

--- ios/ApteligentExample/ApteligentExample/ViewController.swift
+++ ios/ApteligentExample/ApteligentExample/ViewController.swift
@@ -10,16 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+	@IBAction func crash(sender: AnyObject) {
+		let nilValue: AnyObject? = nil
+
+		print(nilValue!)
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
-
 }
 
